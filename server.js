d815f48059c17c44c6c15e3c3bc7dd657618a037
Dongsheng Cai <d@tux.im>
2016-04-16 19:02:05 +1000

    webpack-dev-server

---
+++ server.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+var appconfig = require('./app.config');
+
+var host = appconfig.host;
+var port = appconfig.port;
+
+config.entry.unshift(`webpack-dev-server/client?http://${host}:${port}`);
+
+// webpack/hot/dev-server will reload the entire page if the HMR update fails
+// webpack/hot/only-dev-server reload the page manually
+config.entry.unshift("webpack/hot/dev-server");
+//config.entry.unshift("webpack/hot/only-dev-server");
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+	contentBase: "./build/",
+  publicPath: config.output.publicPath,
+	stats: { colors: true },
+  historyApiFallback: true
+}).listen(port, host, function (err, result) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`Listening at http://${host}:${port}/`);
+});
