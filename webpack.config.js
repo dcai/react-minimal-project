897ebb4fa02e70d321fc9f249e122903d572eaac
Dongsheng Cai <d@tux.im>
2016-04-17 10:14:31 +1000

    fix coding style

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,68 @@
 var path = require('path');
 var webpack = require('webpack');
 
-module.exports = {
-	devtool: 'eval',
-	entry: [
-		'./src/index.jsx'
-	],
-	output: {
+var npmpackage = require('./package.json');
+
+function dirPath(dest){
+  return path.resolve(__dirname, dest);
+}
+
+const debug = process.env.NODE_ENV === 'production' ? false : true;
+
+const config = {
+  devtool: debug ? 'eval' : 'cheap-module-source-map',
+  entry: {
+    vendor: ['react'],
+    index: ['./src/index.jsx']
+  },
+  output: {
     path: path.join(__dirname, 'build'),
-		filename: 'bundle.js',
-		publicPath: '/static/',
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	module: {
-		loaders: [{
-			// match js or jsx
-			test: /\.jsx?$/,
+    filename: '[name].bundle.js',
+    publicPath: '/dist/',
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      filename: 'vendor.bundle.js',
+      chunks: ['index'],
+      minChunks: Infinity,
+    }),
+  ],
+  module: {
+    loaders: [{
+      // match js or jsx
+      test: /\.jsx?$/,
       loaders: ['react-hot', 'babel'],
-			exclude: /node_modules/,
+      exclude: /node_modules/,
       include: path.join(__dirname, 'src')
-		}]
-	},
-	resolve: {
-		extensions: ['', '.js', 'jsx']
-	},
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', 'jsx']
+  },
 };
 
+if(debug) {
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+  );
+} else {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+      },
+      comments: false,
+      sourceMap: false,
+      minimize: true,
+    })
+  );
+}
+
+module.exports = config;
