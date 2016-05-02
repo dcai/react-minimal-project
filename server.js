5618573b59f19837599936716ca8c5e3dcba4618
Dongsheng Cai <dongsheng.cai@news.com.au>
2016-05-02 12:26:53 +1000

    Improvement

--- server.js
+++ server.js
@@ -6,19 +6,20 @@ var appconfig = require('./app.config');
 var host = appconfig.host;
 var port = appconfig.port;
 
-config.entry.unshift(`webpack-dev-server/client?http://${host}:${port}`);
+config.entry.index.unshift(`webpack-dev-server/client?http://${host}:${port}`);
 
 // webpack/hot/dev-server will reload the entire page if the HMR update fails
 // webpack/hot/only-dev-server reload the page manually
-config.entry.unshift("webpack/hot/dev-server");
+config.entry.index.unshift("webpack/hot/dev-server");
 //config.entry.unshift("webpack/hot/only-dev-server");
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  contentBase: "./build/",
-  publicPath: config.output.publicPath,
-  stats: { colors: true },
+  contentBase: "./",
+  stats: {
+    colors: true
+  },
   historyApiFallback: true
 }).listen(port, host, function (err, result) {
   if (err) {
--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hot loading in action.</h1>
+      <h1>Hot loading in action!</h1>
     );
   }
 }
