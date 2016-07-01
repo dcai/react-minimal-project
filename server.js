05df6a8df15e008adb834771090b1e9e9b6eeceb
Dongsheng Cai <dongsheng.cai@news.com.au>
2016-07-01 11:55:52 +1000

    test

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('isomorphic-fetch');
+require('es6-promise').polyfill();
 var express = require('express');
 var helper = require('./devhelper.js');
 var appconfig = require('./app.config');
@@ -8,6 +10,24 @@ var port = appconfig.port;
 var app = express();
 app = helper.applyWebpackMiddleware(app);
 
+app.use('/proxy', (req, res) => {
+  var query = req.query.q;
+  var options = {
+    headers: {
+      'auth-key': '28744ed5982391881611cca6cf5c240',
+      //'Accept': 'application/json',
+      //'Content-Type': 'application/json'
+    },
+  };
+  const api = 'https://digitalapi.auspost.com.au/postcode/search.json';
+  fetch(`${api}?q=${query}`, options).then( response => {
+    console.info(response.headers);
+    return response.json();
+  }).then( json => {
+    console.info(json)
+    res.end('kkk')
+  })
+});
 app.use('/', express.static(__dirname + '/public/'));
 
 app.listen(port, (err, result) => {
--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Location from './location.jsx';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Hot loading in action!</h1>
+        <Location />
       </div>
     );
   }
