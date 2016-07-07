5fe2ab515aabfb971c4904220f1097e126994120
Dongsheng Cai <d@tux.im>
2016-07-07 15:06:16 +1000

    Add swig render engine

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var swig = require('swig');
 var helper = require('./devhelper.js');
 var appconfig = require('./app.config');
 
@@ -6,9 +7,17 @@ var host = process.env.HOST || appconfig.host;
 var port = process.env.PORT || appconfig.port;
 
 var app = express();
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
 app = helper.applyWebpackMiddleware(app);
 
 app.use('/', express.static(__dirname + '/public/'));
+
+app.get('/test', (req, res) => {
+  res.render('test', {});
+})
 
 app.listen(port, host, (err, result) => {
   if (err) {
