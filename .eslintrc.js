b18be27ae48563ace5bf76af4dbd69e928c911a3
Dongsheng Cai <d@tux.im>
2016-11-26 00:15:18 +1100

    .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "root": true,
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "extends": "eslint:recommended",
+    "env": {
+        "mocha": true,
+        "jasmine": true,
+        "jquery": true,
+        "browser": true,
+        "node": true
+    },
+    "rules": {
+        "linebreak-style": ["error", "unix"],
+        "semi": ["error", "always"],
+        "no-console": "off"
+    },
+    "globals": {
+        "console": true
+    }
+};
