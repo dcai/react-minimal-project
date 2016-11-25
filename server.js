3d6fbc7f45ac7868999f7e8b4e0c8f1f361a1457
Dongsheng Cai <d@tux.im>
2016-11-26 00:15:08 +1100

    swig removal

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var swig = require('swig');
+var twig = require('twig');
 var helper = require('./devhelper.js');
 var appconfig = require('./app.config');
 
@@ -7,7 +7,7 @@ var host = process.env.HOST || appconfig.host;
 var port = process.env.PORT || appconfig.port;
 
 var app = express();
-app.engine('html', swig.renderFile);
+app.engine('html', twig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
@@ -20,14 +20,14 @@ app.use('/static', express.static(__dirname + '/static/'));
 
 app.get('/test', (req, res) => {
   res.render('test', {});
-})
+});
 
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   console.error(err.stack);
   res.status(500).json(err);
-})
+});
 
-app.listen(port, host, (err, result) => {
+app.listen(port, host, (err) => {
   if (err) {
     console.log(err);
   }
