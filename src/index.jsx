b11fd44f28236722289cb8dfaca954240123c34e
Dongsheng Cai <d@tux.im>
2017-05-19 09:06:37 +1000

    cleaner hot loading code

--- src/index.jsx
+++ src/index.jsx
@@ -3,22 +3,17 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './app.jsx';
 
-const rootEl = document.getElementById('app');
-ReactDOM.render(
-    <AppContainer><App /></AppContainer>,
-    rootEl
-);
+const render = Component => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('app')
+    );
+};
+
+render(App);
 
 if (module.hot) {
-    module.hot.accept('./app.jsx', () => {
-        // If you use Webpack 2 in ES modules mode, you can
-        // use <App /> here rather than require() a <NextApp />.
-        const NextApp = require('./app.jsx').default;
-        ReactDOM.render(
-            <AppContainer>
-                <NextApp />
-            </AppContainer>,
-            rootEl
-        );
-    });
+    module.hot.accept('./app.jsx', () => render(App));
 }
