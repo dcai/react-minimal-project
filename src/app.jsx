dbb2663cef9e10a9f607386fc34986a5fb27a9a5
Dongsheng Cai <d@tux.im>
2017-09-26 11:41:13 +1000

    Add a button component

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Button from './components/Button.jsx';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Hot loading in action!</h1>
+        <Button/>
       </div>
     );
   }
