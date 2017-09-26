259c2ef7340f502c7d8ff3d643abdf78cb864b12
Dongsheng Cai <d@tux.im>
2017-09-26 11:40:49 +1000

    Cleanup root container hot loading code

--- src/index.jsx
+++ src/index.jsx
@@ -3,22 +3,20 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './app.jsx';
 
-const rootEl = document.getElementById('app');
-ReactDOM.render(
-    <AppContainer><App /></AppContainer>,
-    rootEl
-);
+const domHook = document.getElementById('app');
+const render = Component =>
+    ReactDOM.render(
+        <AppContainer><Component /></AppContainer>,
+        domHook);
+
+render(App);
 
 if (module.hot) {
     module.hot.accept('./app.jsx', () => {
-        // If you use Webpack 2 in ES modules mode, you can
+        // If use Webpack in ES modules mode, you can
         // use <App /> here rather than require() a <NextApp />.
         const NextApp = require('./app.jsx').default;
-        ReactDOM.render(
-            <AppContainer>
-                <NextApp />
-            </AppContainer>,
-            rootEl
-        );
+
+        render(NextApp);
     });
 }
