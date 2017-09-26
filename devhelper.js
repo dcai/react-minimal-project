b5f138d8504e811accc4f9d407b0a2cb77766c41
Dongsheng Cai <d@tux.im>
2017-09-26 11:41:27 +1000

    cleanup devhelper.js

--- devhelper.js
+++ devhelper.js
@@ -1,22 +1,21 @@
 var webpack = require('webpack');
 var devMiddleware = require('webpack-dev-middleware');
 var hotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.config');
+var webpackConfig = require('./webpack.config');
 
-//config.entry.index.unshift(`webpack-dev-server/client?http://${host}:${port}`);
-
+webpackConfig.entry.index.unshift("react-hot-loader/patch");
 // webpack/hot/dev-server will reload the entire page if the HMR update fails
 // webpack/hot/only-dev-server reload the page manually
-config.entry.index.unshift("react-hot-loader/patch");
-config.entry.index.unshift("webpack-hot-middleware/client");
+// webpackConfig.entry.index.unshift(`webpack-dev-server/client?http://${host}:${port}`);
+webpackConfig.entry.index.unshift("webpack-hot-middleware/client");
 
 //enable webpack middleware for hot-reloads in development
-var compiler = webpack(config);
+var compiler = webpack(webpackConfig);
 
 module.exports = {
   applyWebpackMiddleware: (app) => {
-    app.use(devMiddleware(compiler, {
-      publicPath: config.output.publicPath,
+    var devMiddlewareOptions = {
+      publicPath: webpackConfig.output.publicPath,
       noInfo: false,
       quiet: false,
       lazy: false,
@@ -29,10 +28,12 @@ module.exports = {
         chunks: false,
         'errors-only': true
       }
-    }));
-    app.use(hotMiddleware(compiler, {
+    };
+    app.use(devMiddleware(compiler, devMiddlewareOptions));
+    var hotMiddlewareOptions = {
       log: console.log
-    }));
+    };
+    app.use(hotMiddleware(compiler, hotMiddlewareOptions));
 
     return app;
   }
