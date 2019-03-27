7b290704c8e2af4f36e9f00b7e1e43bf954e47ed
Dongsheng Cai <d@tux.im>
2019-03-27 10:15:14 +0000

    fix: npm audit fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 // "off" or 0 - turn the rule off
 // "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)
 // "error" or 2 - turn the rule on as an error (exit code is 1 when triggered)
+const ERROR = 2;
+const WARN = 1;
+const OFF = 0;
 module.exports = {
   root: true,
   extends: ['@friendlyrobot/eslint-config'],
---
+++ babel.config.js
@@ -0,0 +1,24 @@
+const pkg = require('./package.json');
+
+module.exports = function(api) {
+  // Cache the returned value forever and don't call this function again.
+  api.cache(true);
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-runtime',
+    'react-hot-loader/babel',
+  ];
+
+  const presets = [
+    '@babel/preset-react',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['last 2 versions', 'safari >= 7'],
+        },
+      },
+    ],
+  ];
+  return { plugins, presets };
+};
