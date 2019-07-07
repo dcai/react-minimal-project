84d3fd5cbefafabc0ac17403349ab63df65e90a0
Dongsheng Cai <d@tux.im>
2019-07-08 00:36:05 +1000

    feat: add nps

---
+++ package-scripts.js
@@ -0,0 +1,36 @@
+const { concurrent, series, crossEnv } = require('nps-utils');
+
+const envDebug = crossEnv(
+  ['NODE_ENV=development', 'DEBUG=nodejs,watcher'].join(' '),
+);
+const envProd = crossEnv(['NODE_ENV=production'].join(' '));
+
+module.exports = {
+  scripts: {
+    default: 'nps server.dev',
+    server: {
+      dev: `${envDebug} node ./server/main.js`,
+      prod: `${envProd} node ./server/main.js`,
+    },
+    production: {
+      default: series.nps('build', 'server.prod'),
+    },
+    build: {
+      default: `${envProd} webpack`,
+    },
+    lint: {
+      default: 'eslint .',
+      fix: 'eslint --fix .',
+    },
+    test: {
+      default: 'jest',
+      watch: {
+        script: 'jest --watch',
+        description: 'run in the amazingly intelligent Jest watch mode',
+      },
+    },
+    prettier:
+      'prettier --config ./prettier.config.js --write "{server,react,__{tests,mocks}__}/**/*.js"',
+    validate: concurrent.nps('lint', 'test', 'build'),
+  },
+};
